b93ad27774f5bef5de034916d71f85f62f601571
SenpaiSilver <silvermesprims@gmail.com>
2014-06-05 15:35:09 +0200

    Cleaned up HTML.

--- index.php
+++ index.php
@@ -6,19 +6,12 @@
 
 		<link rel="stylesheet"  href="./media/fonts.css" />
 		<link rel="stylesheet"  href="./media/style.css" />
-		<meta name="keywords" content="Silver, SenpaiSilver, Senpai Silver" />
+		<meta name="keywords" content="Silver, SenpaiSilver, Senpai Silver, Field of view, FOV" />
 		<meta name="author" content="Silver" />
 		<meta charset="UTF-8" />
-		
-		<meta name="viewport" content="width=720, initial-scale=1">
 
-		<!--<script src="./media/analytics.js"></script>
-		<script src="./js/knockout-3.1.0.js"></script>
-		<script src="./js/knockout.mapping-latest.js"></script>-->
+		<!--<script src="./media/analytics.js"></script>-->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		
-		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
-		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 		
 		<script src="./js/main.js"></script>
 	</head>
@@ -28,7 +21,7 @@
 				<li class="header-menu"><a href="./">What is FOV ?</a></li>
 			</ul>
 		</div>
-		<h2 id="title" data-bind="text: NAME()">What is FOV ?</h2>
+		<h2 id="title">What is FOV ?</h2>
 		<div id="content">
 			<p>What's so serious ?</p>
 		</div>
@@ -36,14 +29,3 @@
 		</div>
 	</body>
 </html>
-
-		<!--
-			<div id="slide-set">
-				<ul id="slide-list">
-					<li id="slide-element">
-						<label id="slide-text"></label>
-						<img id="slide-picture"/>
-					</li>
-				</ul>
-			</div>
-		-->
